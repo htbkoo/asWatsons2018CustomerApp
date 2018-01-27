e235bede821a552e86e16d3b74b72f6fafbd609c
htbkoo <h1017760@connect.hku.hk>
2018-01-27 17:10:27 +0800

    added Header Prototype - A.S. Watsons Hackathon 2018

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import StickyHeaderFooterScrollView from 'react-native-sticky-header-footer-scroll-view'
 import {Header} from 'react-native-elements';
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
+            <View style={styles.fullScreen}>
                 <Header
                     leftComponent={{icon: 'menu', color: '#fff'}}
                     centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
                     rightComponent={{icon: 'home', color: '#fff'}}
                 />
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
+                <View style={styles.container}>
+                    <Text>Open up App.js to start working on your app!</Text>
+                    <Text>Changes you make will automatically reload.</Text>
+                    <Text>Shake your phone to open the developer menu.</Text>
+                </View>
             </View>
         );
     }
