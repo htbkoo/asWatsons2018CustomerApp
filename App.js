a97293ef7bd22f384fea2442b3116e5f9bab25e6
htbkoo <h1017760@connect.hku.hk>
2018-01-27 15:48:11 +0800

    added <Header/> Prototype - A.S. Watsons Hackathon 2018

--- App.js
+++ App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Header} from 'react-native-elements';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    leftComponent={{icon: 'menu', color: '#fff'}}
+                    centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
+                    rightComponent={{icon: 'home', color: '#fff'}}
+                />
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Changes you make will automatically reload.</Text>
+                <Text>Shake your phone to open the developer menu.</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
