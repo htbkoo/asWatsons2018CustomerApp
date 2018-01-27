fc54bd13562319892f8bd0b7b2bbbe34c8af8199
htbkoo <h1017760@connect.hku.hk>
2018-01-27 19:48:55 +0800

    added StackedNavigationHome.js and TappedNavigationHome.js as examples Prototype - A.S. Watsons Hackathon 2018

---
+++ examples/StackedNavigationHome.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react'
+import Swiper from 'react-native-deck-swiper'
+import {Button, StyleSheet, Text, View} from 'react-native'
+
+import {StackNavigator,} from 'react-navigation';
+import SwipeScreen from "../SwipeScreen";
+
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+
+    render() {
+        const {navigate} = this.props.navigation;
+        return (
+            <Button
+                title="Go to Swipe screen"
+                onPress={navigate.bind(this, 'Swipe', {name: 'Jane'})}
+            />
+        );
+    }
+}
+
+const App = StackNavigator({
+    Home: {screen: HomeScreen},
+    Swipe: {screen: SwipeScreen},
+});
+
+export default App;
