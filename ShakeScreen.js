cff3a7aaf14ee73f4081ec9c33c02df0be26ebba
htbkoo <h1017760@connect.hku.hk>
2018-01-28 11:01:47 +0800

    added back to home button Prototype - A.S. Watsons Hackathon 2018

--- ShakeScreen.js
+++ ShakeScreen.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react'
-import {StyleSheet, View} from 'react-native'
+import {StyleSheet, View, Button} from 'react-native'
 import ShakeSensor from "./ShakeSensor";
 
+import {NavigationActions, withNavigation} from 'react-navigation';
+
+@withNavigation
 class ShakeScreen extends Component {
     static navigationOptions = {
         header: null,
     };
+
+    _resetNavigation(targetRoute) {
+        const resetAction = NavigationActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({routeName: targetRoute}),
+            ],
+        });
+        this.props.navigation.dispatch(resetAction);
+    }
 
     render() {
         console.log("rendered ShakeScreen");
         return (
             <View style={styles.shakeContainer}>
                 <ShakeSensor/>
+                <Button
+                    title="Back to Home"
+                    onPress={this._resetNavigation.bind(this, 'Home')}
+                />
             </View>
         )
     }
@@ -21,7 +38,8 @@ const styles = StyleSheet.create({
     shakeContainer: {
         flex: 1,
         backgroundColor: "#01A89E",
-        padding: "10%"
+        padding: "10%",
+        justifyContent: "space-around"
     },
 });
 
