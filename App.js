df1c98499022c96eb30af7b0a207720ba29d5982
htbkoo <h1017760@connect.hku.hk>
2018-01-28 12:03:24 +0800

    overrode back handler Prototype - A.S. Watsons Hackathon 2018

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {Button, Image, ScrollView, View, Picker} from 'react-native'
-
-import {StackNavigator} from 'react-navigation';
+import {BackHandler, Button, Image, Picker, Platform, ScrollView, View} from 'react-native'
+import {NavigationActions, StackNavigator} from 'react-navigation';
 import SwipeScreen from "./SwipeScreen";
 import ShakeScreen from "./ShakeScreen";
 
@@ -14,6 +13,31 @@ class HomeScreen extends React.Component {
         super(props);
         this.state = {
             location: "island"
+        }
+    }
+
+    componentDidMount() {
+        if (Platform.OS === 'android') {
+            console.log("home - added backListener");
+            BackHandler.addEventListener('hardwareBackPress', () => {
+                let navigation = this.props.navigation;
+                console.log(`pressed back, current at: ${navigation.state.routeName}`);
+                const resetAction = NavigationActions.reset({
+                    index: 0,
+                    actions: [
+                        NavigationActions.navigate({routeName: "Home"}),
+                    ],
+                });
+                navigation.dispatch(resetAction);
+                return true;
+            });
+        }
+    }
+
+    componentWillUnmount() {
+        if (Platform.OS === 'android') {
+            console.log("home - removed backListener");
+            BackHandler.removeEventListener('hardwareBackPress');
         }
     }
 
